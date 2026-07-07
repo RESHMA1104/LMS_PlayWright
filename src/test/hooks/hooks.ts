f6e52039ccc94a79b1de8265c7c724517fec3c51
14-Prasanna <2k22cse115@kiot.ac.in>
2026-07-07 13:32:07 +0530

    Add the comment line to the files

--- src/test/hooks/hooks.ts
+++ src/test/hooks/hooks.ts
@@ -1,20 +1,31 @@
+/*/*
+    Hooks to execute Before and After the Method and entire Test
+*/
+
 import { BugFinder } from "../../world/bug_finder";
 import { Browser, chromium } from "@playwright/test";
-import { Before, After, BeforeAll, AfterAll, Status } from "@cucumber/cucumber";
+import { Before, After, BeforeAll, AfterAll, Status, setDefaultTimeout } from "@cucumber/cucumber";
+
+// Default Timeout
+setDefaultTimeout(60* 1000);
 
 let browser:Browser;
+
+// Browser launch the application 
 BeforeAll(async()=>{
     browser=await chromium.launch({
         headless:true
     })
 })
 
+// Reference to the Object and creating the resource to the CustomWorld
 Before(async function (this:BugFinder) {
     this.browser = browser;
     this.browserContext = await this.browser.newContext();
     this.page = await this.browserContext.newPage();
 })
 
+// If the test Failed ScreenShot capture 
 After(async function (this:BugFinder, {pickle,result}){
     if(result?.status == Status.FAILED && this.page){
         const screenshot = await this.page.screenshot({
@@ -27,6 +38,8 @@ After(async function (this:BugFinder, {pickle,result}){
     await this.browserContext?.close();
 })
 
+
+// Closing all the resource 
 AfterAll(async()=>{
     await browser?.close();
 })
--- src/world/bug_finder.ts
+++ src/world/bug_finder.ts
@@ -1,10 +1,18 @@
+/*/*
+    CustomWorld Creation to use cross the Project 
+*/
+
 import { setWorldConstructor, World } from '@cucumber/cucumber';
 import { Browser, BrowserContext, Page} from '@playwright/test';
 
+
+// CustomWorld extends World
 export class BugFinder extends World{
     browser!:Browser;
     browserContext!:BrowserContext;
     page!:Page;
 }
 
+
+// export the world
 setWorldConstructor(BugFinder);

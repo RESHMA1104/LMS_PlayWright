2625a740e697921c025f8fdf2dd70df8e0ade582
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-07-07 17:06:14 +0530

    Used Env for Add Course Structure Page [URL]

--- src/test/step-definition/addcoursestructure.step.ts
+++ src/test/step-definition/addcoursestructure.step.ts
@@ -1,12 +1,13 @@
 import { Then, When } from "@cucumber/cucumber";
 import { BugFinder } from "../../world/bug_finder";
+import { config } from "../../utils/config";
 
 When('The User Clicks on Course Management in DashboaedPage', async function (this: BugFinder) {
     await this.dashboardPage.clickCourseManagementDrawer();
 });
 
 When('The User Enters The Course Name In Search Bar', async function (this: BugFinder) {
-    await this.coursemanagementPage.enterSearchValue("Bug-Finder")
+    await this.coursemanagementPage.enterSearchValue(config.courseName)
 });
 
 When('The User Clicks on Add course Structure On Search Resulted Course', async function (this: BugFinder) {
@@ -14,5 +15,5 @@ When('The User Clicks on Add course Structure On Search Resulted Course', async 
 });
 
 Then('The User should be redirected to Add Course Structure page', async function (this: BugFinder) {
-    await this.coursemanagementPage.assertCourseManagementUrl();
+    await this.coursemanagementPage.assertCourseManagementUrl(config.addCourseStructureUrl);
 });
